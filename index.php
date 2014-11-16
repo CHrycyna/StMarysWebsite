22888634f6e766299ee0588d184ffd37cf3231c7
Cameron Hrycyna <cameron.hrycyna@gmail.com>
2014-11-15 23:06:52 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<!-- META DATA -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="description" content="St.Marys Nursery & Garden Centre" />
+	<meta name="keywords" content="StMarys,Garden,Nursery,Centre,Plants,Landscaping" />
+	<meta name="author" content="Cameron Hrycyna" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+	<title> St.Mary's Nursery and Garden Centre</title>
+	
+		<!-- JAVASCRIPTS -->
+	<script>
+		function loadCSS(filename)
+		{
+			console.log(filename)
+			var fileref=document.createElement("link")
+			fileref.setAttribute("rel", "stylesheet")
+			fileref.setAttribute("type", "text/css")
+			fileref.setAttribute("href", filename)
+			
+			if (typeof fileref!="undefined")
+				  document.getElementsByTagName("head")[0].appendChild(fileref)
+		}
+	
+		loadCSS("/static/bootstrap/css/bootstrap.css")
+		loadCSS("/static/StyleSheets/base.css")
+		loadCSS("/static/StyleSheets/instagram.css")
+		loadCSS("/static/StyleSheets/Lobster.css")
+		loadCSS("/static/StyleSheets/Slabo27.css")
+	</script>
+	<script src="https://code.jquery.com/jquery.js"></script>
+	<script async src="static/bootstrap/js/bootstrap.js" ></script>
+	<script async src="static/Scripts/twitter.js"> </script>
+	<script async src="static/Scripts/facebook.js"> </script>
+	
+	<link rel="shortcut icon" href="static/imgs/base/stmarys_logo.gif"/>
+	
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<div class="seasonal">
+	  	<div class="container main-content">	
+			<header>
+			<div class="border rounded">>
+				<div class="logo-panel">
+					<img class="logo-image" src="/static/imgs/base/StMarysGardenCentre_Logovector_Blue-White.png"/> 
+				</div>
+				<p class="lead">A Growing Tradition.</p>
+				</div>
+			</header>			
+				
+		 	<div class="contents">
+				<div class="content rounded">
+	  				<h1 class="tri-h2">Our website isn&#39t quite ready.</h1>
+	  				<p> If you have any questions in the meantime, or need to contact us, please contact us using the information below</p>
+	  			</div>
+	  		</div>
+	  		<div class="footer">
+				<div class="row col-wrap footer-content">
+					<div id="footer-col" class="col-md-4">
+						St.Marys Nursery & Garden Centre Ltd.
+						<br>
+			  			2901 St.Mary's Road
+		  				<br>
+		  				Winnipeg, Manitoba, R2N4A6
+		  				<br>
+		  				
+						<br>
+						<div id="contact-information">
+							<a id="contact-email" href="mailto:garden@stmarysnursery.com">Contact Us  </a>
+						 	| 
+							<a id="contact-phone" href="tel:2042557353">  (204) 255-7353</a>
+						</div>
+					</div>
+					<div id="footer-col" class="col-md-4">
+						<img id="footer-logo" src="/static/imgs/base/StMarysGardenCenter_Icon.png"/>
+					</div>
+					<div id="footer-col" class="col-md-4">
+						<div class="social-col">
+						<div class="row ">
+							<a id="instagram-badge" class="social-badge" href="http://instagram.com/stmarysnursery?ref=badge">
+								Instagram
+							</a>
+						</div>
+						<div class="row ">
+							<a id="facebook-badge" class="social-badge" href="https://www.facebook.com/pages/St-Marys-Nursery-and-Garden-Centre-Ltd/128776243867073">
+								Facebook
+							</a>
+						</div>
+						<div class="row ">
+							<a id="twitter-badge" class="social-badge" href="https://twitter.com/StMarysNursery">
+								Twitter
+							</a>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div> <!-- Footer -->
+	  	</div> <!-- Container -->
+	</div> <!-- Seasonal -->
+	
+
+  </body>
+</html>
