1c574dd805981ee9ae1ab31e16fdc28920babc78
Cameron Hrycyna <cameron.hrycyna@gmail.com>
2014-11-15 23:45:16 -0600

    Split header/footer from index.php into header.php and footer.php

---
+++ footer.php
@@ -0,0 +1,47 @@
+	  		<div class="footer">
+				<div class="row col-wrap footer-content">
+					<div id="footer-col" class="col-md-4">
+						St.Marys Nursery & Garden Centre Ltd.
+						<br>
+			  			2901 St.Mary's Road
+		  				<br>
+		  				Winnipeg, Manitoba, R2N4A6
+		  				<br>
+		  				
+						<br>
+						<div id="contact-information">
+							<a id="contact-email" href="mailto:garden@stmarysnursery.com">Contact Us  </a>
+						 	| 
+							<a id="contact-phone" href="tel:2042557353">  (204) 255-7353</a>
+						</div>
+					</div>
+					<div id="footer-col" class="col-md-4">
+						<img id="footer-logo" src="/static/imgs/base/StMarysGardenCenter_Icon.png"/>
+					</div>
+					<div id="footer-col" class="col-md-4">
+						<div class="social-col">
+						<div class="row ">
+							<a id="instagram-badge" class="social-badge" href="http://instagram.com/stmarysnursery?ref=badge">
+								Instagram
+							</a>
+						</div>
+						<div class="row ">
+							<a id="facebook-badge" class="social-badge" href="https://www.facebook.com/pages/St-Marys-Nursery-and-Garden-Centre-Ltd/128776243867073">
+								Facebook
+							</a>
+						</div>
+						<div class="row ">
+							<a id="twitter-badge" class="social-badge" href="https://twitter.com/StMarysNursery">
+								Twitter
+							</a>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div> <!-- Footer -->
+	  	</div> <!-- Container -->
+	</div> <!-- Seasonal -->
+	
+
+  </body>
+</html>
